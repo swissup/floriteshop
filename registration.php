4461d9f361c2a9c3d1c46006c61dfd94b6d1c62c
Roman <support@swissuplabs.com>
2024-08-16 18:27:12 +0300

    Initial

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Local/argento-chic-custom',
+    __DIR__
+);
